a9c10fd1e1649f96fbdb3c9a9b046a31dd383491
Sharvil Shah <sharvil@sharvilshah.com>
2021-04-29 18:24:28 +0530

    debug tags

--- osquery/tables/cloud/aws/ec2_instance_tags.cpp
+++ osquery/tables/cloud/aws/ec2_instance_tags.cpp
@@ -31,6 +31,8 @@ QueryData genEc2InstanceTags(QueryContext& context) {
     return results;
   }
 
+  return results;
+
   std::shared_ptr<ec2::EC2Client> client;
   Status s = makeAWSClient<ec2::EC2Client>(client, region, false);
   if (!s.ok()) {
