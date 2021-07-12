9391c46ee8d7b14f0a1fed702bf9e2841eb84ff7
Sharvil Shah <sharvil@sharvilshah.com>
2021-07-13 02:07:39 +0530

    string_view

--- osquery/remote/http_client.h
+++ osquery/remote/http_client.h
@@ -20,6 +20,8 @@
 #define OPENSSL_NO_MD5 1
 #define OPENSSL_NO_DEPRECATED 1
 
+#define BOOST_BEAST_USE_STD_STRING_VIEW 1
+
 #ifdef WIN32
 // For std:call_once, used below
 #include <mutex>
@@ -34,6 +36,7 @@
 #include <boost/asio/ssl.hpp>
 // clang-format on
 
+#include <string_view>
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/optional/optional.hpp>
